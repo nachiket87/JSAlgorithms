9c7641082befa76336d37cb8716b824f42f3d0a2
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-15 13:37:05 -0400

    :alien:

--- reverseString2.ts
+++ reverseString2.ts
@@ -1,10 +1,12 @@
+// made a function that accepts an array of characters then returns an array of reversed characters
+
 const reverseString = (str: Array<string>) => {
-  let leftIndex: number = 0;
-  let rightIndex: number = str.length - 1;
+  let leftIndex: number = 0; // save the left index number
+  let rightIndex: number = str.length - 1; // save the last index
   while(leftIndex < rightIndex){
     const temp: string = str[leftIndex];
-    // save the current index in temp;
-    str[leftIndex] = str[rightIndex];
+    // save the last index value in temp;
+    str[leftIndex] = str[rightIndex]; // switch it up
     str[rightIndex] = temp;
     // change indexes
     leftIndex++;
