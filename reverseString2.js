ab0770c67434708554155d382207cd585266c170
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-15 13:37:31 -0400

    :alien:

--- reverseString2.js
+++ reverseString2.js
@@ -1,10 +1,11 @@
+// made a function that accepts an array of characters then returns an array of reversed characters
 var reverseString = function (str) {
-    var leftIndex = 0;
-    var rightIndex = str.length - 1;
+    var leftIndex = 0; // save the left index number
+    var rightIndex = str.length - 1; // save the last index
     while (leftIndex < rightIndex) {
         var temp = str[leftIndex];
-        // save the current index in temp;
-        str[leftIndex] = str[rightIndex];
+        // save the last index value in temp;
+        str[leftIndex] = str[rightIndex]; // switch it up
         str[rightIndex] = temp;
         // change indexes
         leftIndex++;
