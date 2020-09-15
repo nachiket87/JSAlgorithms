a488dd918faa37cd1dca3ac79605b31bf5648232
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-15 13:49:57 -0400

    :beers:

--- reverseWords.js
+++ reverseWords.js
@@ -1,8 +1,8 @@
 // reverseWords should print 'steal pound cake'
 var reverseWords = function (message) {
     var temp = message.join("");
-    var yes = temp.split(" ").reverse();
-    console.log(yes.join(" "));
+    var answer = temp.split(" ").reverse();
+    console.log(answer.join(" ")); // hard coded answer perhaps there's a better way to do this.
 };
 var message = ['c', 'a', 'k', 'e', ' ',
     'p', 'o', 'u', 'n', 'd', ' ',
