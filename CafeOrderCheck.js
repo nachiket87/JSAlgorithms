6837e6296bc31b5fe75ea3007a11f5472aadcfde
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-18 19:04:35 -0400

    wasn't the most efficient way. Will check answer :bug:

---
+++ CafeOrderCheck.js
@@ -0,0 +1,26 @@
+// create a function that return whether a servedOrders array is first-come-first-served.
+//below should be true. Comment if you would like to test condition for false and uncomment line 10-13. 
+var takeOutOrders = [17, 8, 24];
+var dineInOrders = [12, 19, 2];
+var servedOrders = [17, 8, 12, 19, 24, 2];
+//below should be false - uncomment to try but don't forget to comment out line 6-8.
+/*const takeOutOrders: Array<number> = [1, 3, 5];
+const dineInOrders: Array<number> = [2, 4, 6];
+let servedOrders: Array<number> = [1, 2, 4, 6, 5, 3]; */
+var orderChecker = function (takeOutOrders, dineInOrders, servedOrders) {
+    var answer = true;
+    servedOrders.forEach(function (order) {
+        if (order === takeOutOrders[0]) {
+            takeOutOrders.shift();
+        }
+        else if (order === dineInOrders[0]) {
+            dineInOrders.shift();
+        }
+        else {
+            answer = false;
+            return 0;
+        }
+    });
+    return answer;
+};
+console.log(orderChecker(takeOutOrders, dineInOrders, servedOrders));
