cc946f43b516401e38d479ea72077ad031e6e72a
Nachiket Pusalkar <nachiket87@gmail.com>
2020-11-28 00:33:38 -0500

    shortestpath

---
+++ shortest.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.connections = [];
+  }
+}
+
+const a = new Node('start');
+const b = new Node('b');
+const c = new Node('c');
+a.connections.push(b);
+a.connections.push(c);
+x = new Node('finish');
+c.connections.push(x);
+
+let current = a;
+let visited = [];
+let toVisit = [];
+let i = 0;
+while (current.value !== 'finish') {
+  visited.push(current);
+  toVisit = [...current.connections];
+  current = toVisit[0] ? toVisit[0] : c;
+  toVisit.splice(1, toVisit.length - 1);
+  i++;
+}
+
+console.log(visited);
