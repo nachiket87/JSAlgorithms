2b32bae5c2773e8412ce61ead868d19cf3062b67
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-08 01:01:27 -0500

    this implemtnation

--- FE/this.js
+++ FE/this.js
@@ -1,4 +1,5 @@
-const person = {
+//implicit binding
+/*const person = {
   name: 'Nachiket',
   saymyName() {
     console.log(`My name is ${this.name}`);
@@ -6,4 +7,28 @@ const person = {
 };
 
 person.saymyName();
-// this keyword takes name
+// this keyword takes name */
+
+const person = {
+  name: 'Nachiket',
+};
+const superHero = {
+  heroname: 'Batman',
+};
+
+function saymyName(last) {
+  console.log(`My name is ${this.name} and superhero name is ${last}`);
+}
+
+//const sayNachiName = saymyName.bind(superHero);
+// binding will let this keyword know what the context is
+
+function bind(fn, context) {
+  return function () {
+    fn.apply(context, [...arguments]);
+  };
+}
+
+boundFn = bind(saymyName, person);
+
+boundFn('Batman');
