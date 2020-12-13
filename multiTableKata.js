9a48b68f028e1fb5b749e5f8ec60b9c5ed87f1a5
Nachiket Pusalkar <nachiket87@gmail.com>
2020-12-12 20:22:37 -0500

    FUCK

---
+++ multiTableKata.js
@@ -0,0 +1,21 @@
+/*multiplicationTable = function (size) {
+  // [[],[],[]]
+  let outerArray = [];
+  for (let i = 1; i <= size; i++) {
+    const innerArray = [];
+    for (let j = 1; j <= size; j++) {
+      innerArray.push(i * j);
+    }
+    outerArray.push(innerArray);
+  }
+  return outerArray;
+};
+
+//console.log(multiplicationTable(3));
+
+const opposite = (number) => {
+  return 0 - number;
+};
+
+console.log(34);
+*/
